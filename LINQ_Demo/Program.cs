804c168d080b1fe4d00c98077b111bc918f7b530
Salojakuikel <salojakuikel@gmail.com>
2024-11-27 13:09:24 +0545

    simple linq demo code

---
+++ LINQ_Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Demo
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] age = { 10, 12, 15, 4,10,12,18,20,22};
+            var a = from i in age where i >15 select i;
+
+            foreach(int item in a)
+            {
+              Console.WriteLine(item);  
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
